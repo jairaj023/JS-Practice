21ec3661503d0b611abe1ac29095f60c89d0423a
Jairaj Choudhary <choudharyjairaj569@gmail.com>
2026-02-16 23:25:30 +0530

    Add files via upload

---
+++ java s/script.js
@@ -0,0 +1,103 @@
+function sum() {
+    a = 5; b = 6;
+        console.log(a+b);
+    }
+   
+    sum();
+
+function sub() {
+    a = 7; b = 4;
+    console.log(a-b);
+}
+
+sub();
+
+function mul() {
+    a = 23; b = 3;
+    console.log(a*b); 
+}
+
+mul();
+
+function div() {
+    a = 100; b = 3;
+    console.log(a/b);
+}
+
+div();
+
+function mod() {
+    a = 100; b = 69;
+    console.log(a%b);
+}
+
+mod();
+
+function SI() {
+    p = 69; r = 82; t = 97;
+    console.log(p*r*t)/100;
+}
+
+SI();
+
+//Area
+
+function Tri() {
+    b =  5; h = 4;
+    console.log(1/2*b*h);
+}
+
+Tri();
+
+function Rac() {
+    l = 11; b = 21;
+    console.log(l*b);
+}
+
+Rac();
+
+function square() {
+    a = 51;
+    console.log(a*a);
+}
+
+square();
+
+function circle() {
+    r = 11;
+    console.log(3.14*r*r);
+}
+
+circle();
+
+function swap() {
+    a = 69; b = 99;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    document.write("a = ", a);
+    document.write("<br>b = ", b);
+}
+
+swap();
+
+function Swap() {
+    a = parseInt(prompt("Enter the value: "));
+    b = parseInt(prompt("Enter the value: "));
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    document.write("<br>a = ", a);
+    document.write("<br>b = ", b);
+}
+
+Swap();
+
+function dynamic_sum() {
+    a = parseInt(prompt("Enter the value: "));
+    b = parseInt(prompt("Enter the value: "));
+    alert(a+b);
+    }
+   
+    dynamic_sum();
+
